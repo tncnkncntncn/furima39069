99b991ec5a2d7d8a938180eec56196876322c5bf
渡辺友美 <tncnkncn.0329@gmail.com>
2023-03-22 18:34:26 +0900

    アクティブハッシュの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:price,:description, :category_id,:condition_id, :shipping_id,:region_id,:day_to_ship_id,)
+  end
+  
 end
