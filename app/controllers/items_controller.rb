71a20fa86bb9bd64487ba2d9375bf2e7072ceaaa
渡辺友美 <tncnkncn.0329@gmail.com>
2023-03-27 15:55:33 +0900

    コントローラーの記述修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit,:destroy]
   before_action :set_item, only: [:show, :edit, :update,:destroy]
   
   def index
@@ -42,9 +42,10 @@ class ItemsController < ApplicationController
     if @item.user == current_user
     @item.destroy
     redirect_to root_path
-  end
+    else
+  redirect_to root_path
 end
-
+end
 
 
   private
