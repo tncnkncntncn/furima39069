b20e07f40abdbebc116b1797a4eb85feb11fa700
渡辺友美 <tncnkncn.0329@gmail.com>
2023-03-30 17:49:47 +0900

    トップページ　売り切れの場合はsoldoutを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit,:destroy]
   before_action :set_item, only: [:show, :edit, :update,:destroy]
-  
+
   def index
     @item = Item.order(created_at: :desc)
   end
