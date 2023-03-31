31fcbbc95e41f8d8e05723fa69ad078bfa60a1f9
渡辺友美 <tncnkncn.0329@gmail.com>
2023-03-31 15:57:16 +0900

    テストコード修正

--- spec/factories/order_address.rb
+++ spec/factories/order_address.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :order_address do
-    item_id{1}
-    user_id{1}
     postal_code {'123-4567'}
     region_id { 2 }
     city {'シガンシナ区'}
@@ -10,6 +8,8 @@ FactoryBot.define do
     phone_number {'09012341234'}
     token {"tok_abcdefghijk00000000000000000"}
 
+    association :user
+    association :item
  
   end
 end
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe OrderAddress, type: :model do
   describe '商品購入' do
   before do
-    @user = FactoryBot.build(:user)
-    @item = FactoryBot.build(:item)
-    @order_address = FactoryBot.build(:order_address, user_id: @user, item_id: @item)
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
+    @order_address = FactoryBot.build(:order_address, user_id: @user.id, item_id: @item.id)
   end
 
   context '内容に問題ない場合' do
