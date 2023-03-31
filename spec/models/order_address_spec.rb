c7e5847c010c76060aa655ed900cb78ebd673ef6
渡辺友美 <tncnkncn.0329@gmail.com>
2023-03-31 18:25:40 +0900

    テストコードの修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -3,8 +3,8 @@ RSpec.describe OrderAddress, type: :model do
   describe'商品購入' do
     before do
       user = FactoryBot.create(:user)
-      @item = FactoryBot.create(:item, user_id: user.id)
-      @order_address = FactoryBot.build(:order_address,item_id: @item.id,user_id: @item.user.id)
+      item = FactoryBot.create(:item)
+      @order_address = FactoryBot.build(:order_address,user_id: user.id,item_id: item.id)
     end
 
   context "購入ができる時" do
