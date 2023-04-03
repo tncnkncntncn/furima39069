aa67cba553dab3c1f2c4106304bec8363f25bda2
渡辺友美 <tncnkncn.0329@gmail.com>
2023-04-03 12:24:32 +0900

    販売手数料、利益の反映

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,6 @@
 require("@rails/ujs").start()
-// require("turbolinks").start() //
+//require("turbolinks").start() //
 require("@rails/activestorage").start()
 require("channels")
+require("../item_price")
 require("../card")
