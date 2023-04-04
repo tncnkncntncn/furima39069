21831f7119e6c887b8b600633a792cc4711ac26d
渡辺友美 <tncnkncn.0329@gmail.com>
2023-04-05 02:11:41 +0900

    Ec2移行のため修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,7 +6,7 @@ app_path = '/var/www/furima39069'
 worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
-working_directory "/var/www/furima39069"
+working_directory "/var/www"
 
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/tmp/pids/unicorn.pid"
