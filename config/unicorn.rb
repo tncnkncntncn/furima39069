e87a7d556b8fb2f55ef5c0e7f960181f913fb97e
渡辺友美 <tncnkncn.0329@gmail.com>
2023-04-04 20:03:03 +0900

    EC2移行作業

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,7 +7,7 @@ stderr_path "#{app_path}/log/unicorn.stderr.log"
 worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
-working_directory "#{app_path}/current"  # 「current」を指定
+working_directory "/home/ec2-user/furima39069"  # 「current」を指定
 
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"  # 「shared」の中を参照するよう変更
