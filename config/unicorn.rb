a51c473ba2acedb3cf2fa58e1909afbcab3ebdd9
渡辺友美 <tncnkncn.0329@gmail.com>
2023-04-05 10:25:37 +0900

    EC2移行作業

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,21 +1,17 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-#app_path = File.expand_path('../../', __FILE__)
-app_path = '/var/www/furima39069'
+app_path = File.expand_path('../../', __FILE__)
 
 #アプリケーションサーバの性能を決定する
 worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
-working_directory "/var/www/furima39069"
-#working_directory File.expand_path('../../', __FILE__)
-
-
+working_directory app_path
 
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
-listen 3000
+listen "#{app_path}/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
