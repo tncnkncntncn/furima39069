39964fcc93f425ea1480ee97d080eb646deebd6b
渡辺友美 <tncnkncn.0329@gmail.com>
2023-04-05 19:32:51 +0900

    EC2移行作業

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '52.198.88.49:3000', user: 'ec2-user', roles: %w{app db web}
+server '13.230.104.185', user: 'ec2-user', roles: %w{app db web}
